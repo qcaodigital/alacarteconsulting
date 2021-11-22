92d1e66ee58beb9aab9efd5658da9cb1c446de15
qcaodigital <qcao.digital@gmail.com>
2021-11-21 23:52:51 -0500

    wip

--- components/nav/HamburgerMenu.tsx
+++ components/nav/HamburgerMenu.tsx
@@ -1,6 +1,6 @@
 import { INavItem, sitemap } from 'utils/sitemap';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
@@ -32,48 +32,56 @@ export function HamburgerMenu({ toggleHbm }: HamburgerMenuProps) {
 			{...transition}
 		>
 			<div className='absolute inset-0 bg-home-hero bg-cover border-[6px] border-white opacity-20' />
-			<ul className='absolute-center text-center flex flex-col gap-3'>
-				{activeLinks
-					.filter((item) => item.labelAsString || typeof item.label === 'string')
-					.map((item) => (
+			<AnimatePresence>
+				<motion.ul
+					key={activeLinks[0].href}
+					className='absolute-center text-center flex flex-col gap-3'
+					initial={{ opacity: 0 }}
+					exit={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+				>
+					{activeLinks
+						.filter((item) => item.labelAsString || typeof item.label === 'string')
+						.map((item) => (
+							<li
+								key={item.href}
+								className={`font-mon text-lg uppercase transition duration-200 | hover:opacity-75 ${
+									item.color ? item.color : ''
+								} ${item.label === 'Contact' ? 'text-white bg-black py-1.5' : ''}`}
+								onClick={!item.subpaths ? toggleHbm : undefined}
+							>
+								{item.subpaths ? (
+									<div
+										className='cursor-pointer'
+										onClick={() => {
+											if (item.subpaths) {
+												setActiveLinks(item.subpaths);
+												setCurrentBasePath(item.href);
+											}
+										}}
+									>
+										{item.labelAsString || item.label}
+									</div>
+								) : (
+									<Link href={(currentBasePath ?? '') + item.href}>
+										{item.labelAsString || item.label}
+									</Link>
+								)}
+							</li>
+						))}
+					{activeLinks !== defaultLinks && (
 						<li
-							key={item.href}
-							className={`font-mon text-lg uppercase transition duration-200 | hover:opacity-75 ${
-								item.color ? item.color : ''
-							} ${item.label === 'Contact' ? 'text-white bg-black py-1.5' : ''}`}
-							onClick={!item.subpaths ? toggleHbm : undefined}
+							onClick={() => {
+								setActiveLinks(defaultLinks);
+								setCurrentBasePath(null);
+							}}
+							className='cursor-pointer transition duration-200 | hover:opacity-75'
 						>
-							{item.subpaths ? (
-								<div
-									className='cursor-pointer'
-									onClick={() => {
-										if (item.subpaths) {
-											setActiveLinks(item.subpaths);
-											setCurrentBasePath(item.href);
-										}
-									}}
-								>
-									{item.labelAsString || item.label}
-								</div>
-							) : (
-								<Link href={(currentBasePath ?? '') + item.href}>
-									{item.labelAsString || item.label}
-								</Link>
-							)}
+							<FontAwesomeIcon icon={faArrowLeft} />
 						</li>
-					))}
-				{activeLinks !== defaultLinks && (
-					<li
-						onClick={() => {
-							setActiveLinks(defaultLinks);
-							setCurrentBasePath(null);
-						}}
-						className='cursor-pointer transition duration-200 | hover:opacity-75'
-					>
-						<FontAwesomeIcon icon={faArrowLeft} />
-					</li>
-				)}
-			</ul>
+					)}
+				</motion.ul>
+			</AnimatePresence>
 		</motion.section>
 	);
 }
---
+++ pages/work/index.tsx
@@ -0,0 +1,8 @@
+export default function Work() {
+	return (
+		<main className='layout-tall bg-cover bg-home-hero relative'>
+			<div className='absolute inset-0 z-0 bg-buttercream/80' />
+			<div className='relative'>Coming soon.</div>
+		</main>
+	);
+}
